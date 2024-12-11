dbbcafbe854d6ccf8cdaece026cfbc9acd3c7646
Mike Haiduk <x29a100@gmail.com>
2024-12-11 17:50:55 +0100

    funnel by sessions

--- send_to_address.php
+++ send_to_address.php
@@ -5,7 +5,8 @@ $from_address = get_required(from_address);
 $to_address = get_required(to_address);
 $message = get_required(message);
 $template = get_required(template);
+$site_domain = get_required(site_domain);
 
-$response[success] = messageSendToAddress($from_address, $to_address, $message, $template);
+$response[success] = messageSendToAddress($from_address, $to_address, $message, $template, $site_domain);
 
 commit($response);
